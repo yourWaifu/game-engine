16f7d3059797f87ea6351b755cd719f38f48c97f
Sleepy Flower Girl <wuhao64@gmail.com>
2021-11-24 23:38:28 -0500

    Rewriten version recorded on video

--- js/index.ts
+++ js/index.ts
@@ -1,67 +1,66 @@
 import * as three from 'three';
 
-import("../pkg/index").then(module => {
-    console.log(module.sum(new Int32Array([1, 1, 1])));
-    var app = new App();
-});
-
 class App {
 
-    camera: three.PerspectiveCamera;
     scene: three.Scene;
+    camera: three.PerspectiveCamera;
     renderer: three.WebGLRenderer;
 
-    mesh: three.Mesh | undefined;
-    frameNum: number;
-    frameCount: number;
-    frameSize: number;
+    renderTask: () => void;
+
+    frameNum: number = 0;
+    readonly frameSize: number = 16/512;
 
     constructor() {
-        this.camera = new three.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-        this.camera.position.z = 500;
-        
         this.scene = new three.Scene();
         this.scene.background = new three.Color("gray");
 
-        const meshPromise = new Promise((resolve, reject) => {
-            new three.TextureLoader().load('compass.png', (texture) => {
-                const geometry = new three.PlaneGeometry(600, 600);
-                const material = new three.MeshBasicMaterial( { map: texture } );
+        this.camera = new three.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+        this.camera.position.z = 500;
 
-                this.frameSize = 16/512;
-                this.frameCount = 512/16;
-                this.frameNum = 0;
-                texture.repeat.set(1, this.frameSize);
-                texture.magFilter = three.NearestFilter;
-                material.alphaTest = 0.5;
-
-                this.mesh = new three.Mesh(geometry, material);
-                resolve(this.mesh);
-            });
-        });
-        
         this.renderer = new three.WebGLRenderer();
-        this.renderer.setPixelRatio( window.devicePixelRatio );
+        this.renderer.setPixelRatio(window.devicePixelRatio);
         this.resize();
-        document.body.appendChild( this.renderer.domElement );
-
-        meshPromise.then((mesh) => {
-            this.scene.add(this.mesh);
-        });
+        document.body.appendChild(this.renderer.domElement);
 
         //start animating
         this.animate = this.animate.bind(this);
         this.resize = this.resize.bind(this);
         window.addEventListener("resize", this.resize);
         window.requestAnimationFrame(this.animate);
+
+        const meshPromise = new Promise<three.Mesh>((resolve, reject) => {
+            new three.TextureLoader().load('compass.png', (texture) => {
+                const geometry = new three.PlaneGeometry(600, 600);
+                const material = new three.MeshBasicMaterial( {map: texture } );
+
+                var mesh = new three.Mesh(geometry, material);
+
+                this.scene.add(mesh);
+                resolve(mesh);
+
+                texture.repeat.set(1, this.frameSize);
+
+                texture.magFilter = three.NearestFilter;
+
+                material.alphaTest = 0.5;
+            })
+        });
+
+        meshPromise.then((mesh: three.Mesh) => {
+            this.renderTask = () => {
+                this.frameNum += 1;
+                (mesh.material as three.MeshBasicMaterial)?.map.offset.set(0, 1 - this.frameSize * this.frameNum);
+                const frameCount = 512/16;
+                this.frameNum = this.frameNum % frameCount;
+            };
+        });
     }
 
     animate() {
         window.requestAnimationFrame(this.animate);
 
-        this.frameNum += 1;
-        this.frameNum = this.frameNum % this.frameCount;
-        (this.mesh?.material as three.MeshBasicMaterial)?.map.offset.set(0, 1 - (this.frameSize * (this.frameNum + 1)));
+        this.renderTask?.();
 
         this.renderer.render(this.scene, this.camera);
     }
@@ -71,10 +70,12 @@ class App {
         const width = window.innerWidth * pixelRatio;
         const height = window.innerHeight * pixelRatio;
         const aspect = width / height;
-        
+
         this.camera.aspect = aspect;
         this.camera.updateProjectionMatrix();
 
         this.renderer.setSize(width, height);
     }
 }
+
+var app = new App();
